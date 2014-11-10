27e27a870e1f1404b2f8c1356a1b90d0f9cf4f20
MGeraght <mgeraght@ebmud.com>
2014-11-10 13:57:36 -0800

    done with demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 DemoApp::Application.routes.draw do
+  resources :microposts
+
+  resources :users
+
+  resources :names
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
